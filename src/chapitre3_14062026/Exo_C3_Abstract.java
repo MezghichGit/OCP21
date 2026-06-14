c545f135aa7daebd130c4b5b43deb1ed864b13b2
Mohamed Amine MEZGHICH <git@mezghich.com>
2026-06-14 17:29:05 +0100

    correction exo

---
+++ src/chapitre3_14062026/Exo_C3_Abstract.java
@@ -0,0 +1,17 @@
+package chapitre3_14062026;
+
+public class Exo_C3_Abstract {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		StringBuilder s2 = new StringBuilder("java");
+
+		if (s1 == s2)
+		    System.out.print("1");
+
+		if (s1.equals(s2))
+		    System.out.print("2");
+
+	}
+
+}
